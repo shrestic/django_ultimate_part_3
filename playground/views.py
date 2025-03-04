499369d06efe216d0251fabb1dfd3c015c02d963
shrestic <nhatphongcgp@gmail.com>
2025-03-04 14:36:46 +0700

    Sending Emails

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    try:
+        # send_mail("Subject here", "Here is the message.", "info@moshby.com", ["bob@moshby.com"])
+        mail_admins("Subject here", "Here is the message.",html_message="<h1>HTML Message</h1>")
+    except BadHeaderError:
+        pass
+    return render(request, "hello.html", {"name": "Mosh"})
--- storefront/settings.py
+++ storefront/settings.py
@@ -168,3 +168,14 @@ DJOSER = {
 }
 
 SIMPLE_JWT = {"AUTH_HEADER_TYPES": ("JWT",), "ACCESS_TOKEN_LIFETIME": timedelta(days=1)}
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "localhost"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = "from@moshby.com"
+
+
+ADMINS = [("Mosh", "admin@moshby.com")]
