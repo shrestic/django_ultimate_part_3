45b452b8d532d34c86e9436458f62d2792a981ed
shrestic <nhatphongcgp@gmail.com>
2025-03-04 10:39:56 +0700

    Returning Images from the API

--- store/views.py
+++ store/views.py
@@ -35,7 +35,7 @@ from .serializers import (
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+    queryset = Product.objects.prefetch_related("images").all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
