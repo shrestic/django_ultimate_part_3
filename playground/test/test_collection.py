00ea18a4eb245de5860d9decde61b7e41969588a
shrestic <nhatphongcgp@gmail.com>
2025-03-05 16:25:18 +0700

    Fixtures

--- playground/test/test_collection.py
+++ playground/test/test_collection.py
@@ -1,38 +1,41 @@
-import pytest
-from rest_framework.test import APIClient
 from rest_framework import status
-from django.contrib.auth.models import User
+import pytest
+
+
+@pytest.fixture
+def create_collection(api_client):
+    def do_create_collection(collection):
+        return api_client.post("/store/collections/", collection)
+
+    return do_create_collection
 
 
 @pytest.mark.django_db
 class TestCreateCollection:
-    def test_if_user_is_anonymous_returns_400(client):
-        client = APIClient()
-        response = client.post("/store/collections/", data={"title": "a"})
+    def test_if_user_is_anonymous_returns_401(self, create_collection):
+        response = create_collection({"title": "a"})
+
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_not_admin_returns_403(client):
-        client = APIClient()
-        """
-        force_authenticate là một phương thức của APIClient trong Django REST Framework.
-         - Nó dùng để "ép buộc" client được xác thực (authenticated) với một user cụ thể mà không cần qua quá trình login thật (như gửi username/password).
-         - user={}: Ở đây, thay vì truyền một đối tượng User thực sự (ví dụ: User.objects.create()), người viết truyền một dictionary rỗng {}. Điều này có nghĩa là client được "xác thực" với một user không hợp lệ hoặc không có thật (không có thông tin gì cả).
-         Ý nghĩa: Dòng này mô phỏng một tình huống mà request được gửi từ một user không có quyền hoặc không được xác thực đầy đủ. Nó kiểm tra xem API có từ chối request từ user "giả" này không.
-        """
-        client.force_authenticate(user={})
-        response = client.post("/store/collections/", data={"title": "a"})
+    def test_if_user_is_not_admin_returns_403(self, authenticate, create_collection):
+        authenticate()
+
+        response = create_collection({"title": "a"})
+
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_data_is_invalid_return_400(client):
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-        response = client.post("/store/collections/", data={"title": ""})
+    def test_if_data_is_invalid_returns_400(self, authenticate, create_collection):
+        authenticate(is_staff=True)
+
+        response = create_collection({"title": ""})
+
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data["title"] is not None
 
-    def test_if_data_is_invalid_return_201(client):
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-        response = client.post("/store/collections/", data={"title": "a"})
+    def test_if_data_is_valid_returns_201(self, authenticate, create_collection):
+        authenticate(is_staff=True)
+
+        response = create_collection({"title": "a"})
+
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data["id"] > 0
