149ab36433b2a10d23051328fb19374bc1cf0c40
shrestic <nhatphongcgp@gmail.com>
2025-03-07 10:28:22 +0700

    Using the Low-level Cache API

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.core.cache import cache
 import requests
 
 
 def say_hello(request):
-    requests.get("http://httpbin.org/delay/2")
-    return render(request, "hello.html", {"name": "Mosh"})
+    key = "httpbin_result"
+    if cache.get(key) is None:
+        response = requests.get("http://httpbin.org/delay/2")
+        data = response.json()
+        cache.set(key, data, 10)
+    return render(request, "hello.html", {"name": cache.get(key)})
