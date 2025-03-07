f109571a5bebf0fea01d1ad0c05db12c8d00ad9b
shrestic <nhatphongcgp@gmail.com>
2025-03-07 10:53:11 +0700

    Managing Redis Cache Content

--- storefront/settings.py
+++ storefront/settings.py
@@ -199,7 +199,7 @@ CELERY_BEAT_SCHEDULE = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://127.0.0.1:6379/2",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
