dde71f6ac5986005db1f6b07dcc6458730186b76
shrestic <nhatphongcgp@gmail.com>
2025-03-08 15:18:17 +0700

    Logging

--- playground/views.py
+++ playground/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render
-from django.utils.decorators import method_decorator
-from django.views.decorators.cache import cache_page
 from rest_framework.views import APIView
 import requests
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class HelloView(APIView):
-    @method_decorator(cache_page(5 * 60))
     def get(self, request):
-        response = requests.get("http://httpbin.org/delay/2")
-        data = response.json()
-        return render(request, "hello.html", {"name": data})
-
-
-# @cache_page(5 * 60)
-# def say_hello(request):
-#     response = requests.get("http://httpbin.org/delay/2")
-#     data = response.json()
-#     return render(request, "hello.html", {"name": "Mosh"})
+        try:
+            logger.info("Calling httpbin")
+            response = requests.get("https://httpbin.org/delay/2")
+            logger.info("Received the response")
+            data = response.json()
+        except requests.ConnectionError:
+            logger.critical("httpbin is offline")
+        return render(request, "hello.html", {"name": "Mosh"})
--- storefront/settings.py
+++ storefront/settings.py
@@ -213,18 +213,23 @@ LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
+        "console": {"class": "logging.StreamHandler"},
+        "file": {
+            "class": "logging.FileHandler",
+            "filename": "general.log",
+            "formatter": "verbose",
         },
-        "file": {"class": "logging.FileHandler", "filename": "general.log"},
     },
     "loggers": {
         "": {
             "handlers": ["console", "file"],
-            "level": os.environ.get("DJANGO_LOG_LEVEL", "INFO"),
-        },
+            "level": os.environ.get(
+                "DJANGO_LOG_LEVEL",
+                "INFO",
+            ),
+        }
     },
-    "formatter": {
+    "formatters": {
         "verbose": {
             "format": "{asctime} ({levelname}) - {name} - {message}",
             "style": "{",
