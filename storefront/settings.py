609e0184675d9d5f51826210e20e56d09599e17a
shrestic <nhatphongcgp@gmail.com>
2025-03-08 15:05:22 +0700

    Configuring Logging

--- storefront/settings.py
+++ storefront/settings.py
@@ -208,3 +208,26 @@ CACHES = {
         },
     }
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+        "file": {"class": "logging.FileHandler", "filename": "general.log"},
+    },
+    "loggers": {
+        "": {
+            "handlers": ["console", "file"],
+            "level": os.environ.get("DJANGO_LOG_LEVEL", "INFO"),
+        },
+    },
+    "formatter": {
+        "verbose": {
+            "format": "{asctime} ({levelname}) - {name} - {message}",
+            "style": "{",
+        },
+    },
+}
